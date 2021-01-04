3218f216055a75c83767d1b65735ccc7f9f7dac4
chemthan <kms.vudang@gmail.com>
2021-01-04 23:01:52 +0700

    Refactor useEffect with cleanup

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,20 +4,55 @@ const API_ENDPOINT = 'https://en.wikipedia.org/w/api.php';
 
 const Search = () => {
     const [term,setTerm] = useState("");
+    const [results,setResults] = useState([]);
+
+
     useEffect(() => {
-        (async () => {
-            await 
-            axios.get(API_ENDPOINT,{
-                params:{
-                    action:'query',
-                    list:'search',
-                    origin:'*',
-                    format:'json',
-                    srsearch:term
-                }
-            })
-        })() 
+        console.log('rerender');
+        const timeId = setTimeout(() => {
+            if(term){
+                (async () => {
+                    const {data} = await 
+                    axios.get(API_ENDPOINT,{
+                        params:{
+                            action:'query',
+                            list:'search',
+                            origin:'*',
+                            format:'json',
+                            srsearch:term
+                        }
+                    })
+                    setResults(data.query.search)
+                })() 
+            }
+        },500);
+
+        // Cleanup
+        return () => {
+            console.log('cleanup');
+           clearTimeout(timeId)
+        }
+
     },[term])
+
+    const renderedResults = results.map((result) => {
+        return(
+            <div key={result.pageid} className="item">
+                <div className="right floated content">
+                    <a 
+                    className="ui button"
+                    href={`https://en.wikipedia.org?curid=${result.pageid}`}
+                    >VISIT</a>
+                </div>
+                <div className="content">
+                    <div className="header">
+                        {result.title}
+                    </div>
+                    <span dangerouslySetInnerHTML={{__html:result.snippet}}></span>
+                </div>
+            </div>
+        )
+    })
 
     return(
         <div>
@@ -29,6 +64,9 @@ const Search = () => {
                     onChange={(e) => setTerm(e.target.value)}
                     className="input"/>
                 </div>
+            </div>
+            <div className="ui celled list">
+                {renderedResults}
             </div>
         </div>
     )
