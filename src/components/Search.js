02c52f5d4210e8a94d1f2844d5e7c8c6e5f73b78
chemthan <kms.vudang@gmail.com>
2021-01-04 22:13:02 +0700

    Update syntax ES6

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,28 +1,23 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-
-const API_URL = 'https://en.wikipedia.org/w/api.php';
-
-
+const API_ENDPOINT = 'https://en.wikipedia.org/w/api.php';
 
 const Search = () => {
     const [term,setTerm] = useState("");
-
- 
     useEffect(() => {
-      (fetch(API_URL,{
-          params:{
-              action:'query',
-              list:'search',
-              origin:'*',
-              format:'json',
-              srsearch:term
-          }
-      })).then(data => console.log(data))
-      ()
-      
+        (async () => {
+            await 
+            axios.get(API_ENDPOINT,{
+                params:{
+                    action:'query',
+                    list:'search',
+                    origin:'*',
+                    format:'json',
+                    srsearch:term
+                }
+            })
+        })() 
     },[term])
-
 
     return(
         <div>
